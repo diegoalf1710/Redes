0a6d56f4d462706ebad5bbbc7cbcd067d1927915
Diego Alfonso <124203169+diegoalf1710@users.noreply.github.com>
2025-02-19 17:57:05 -0600

    ñ

--- Assets/PlayerNameDisplay.cs
+++ Assets/PlayerNameDisplay.cs
@@ -1,32 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Añade esta línea para usar TextMeshPro
 using Photon.Pun;
-using TMPro;
 
 public class PlayerNameDisplay : MonoBehaviourPun
 {
+    public GameObject nameLabelPrefab; // Prefab del nombre de usuario (TextMeshPro)
+    private GameObject nameLabel; // Instancia del nombre de usuario
 
-    public GameObject nameLabelPrefab;
-    private GameObject nameLabel;
-    // Start is called before the first frame update
     void Start()
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
         {
+            // Solo el dueño del PhotonView instancia el nombre de usuario
             nameLabel = Instantiate(nameLabelPrefab, Vector3.zero, Quaternion.identity);
             nameLabel.transform.SetParent(GameObject.Find("Canvas").transform, false);
 
+            // Asignar el nombre de usuario al texto
             TextMeshProUGUI nameText = nameLabel.GetComponent<TextMeshProUGUI>();
             nameText.text = photonView.Owner.NickName;
 
-            nameText.color = Color.green;
+            // Asignar un color diferente para el jugador local
+            nameText.color = Color.green; // Puedes cambiar el color según tus preferencias
         }
         else
         {
+            // Para otros jugadores, instanciar el nombre de usuario
             nameLabel = Instantiate(nameLabelPrefab, Vector3.zero, Quaternion.identity);
             nameLabel.transform.SetParent(GameObject.Find("Canvas").transform, false);
 
+            // Asignar el nombre de usuario al texto
             TextMeshProUGUI nameText = nameLabel.GetComponent<TextMeshProUGUI>();
             nameText.text = photonView.Owner.NickName;
         }
@@ -34,10 +36,20 @@ public class PlayerNameDisplay : MonoBehaviourPun
 
     void Update()
     {
-        if(nameLabel != null)
+        if (nameLabel != null)
         {
-            Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2f);
+            // Actualizar la posición del nombre de usuario en la pantalla
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2f); // Ajusta la altura del texto
             nameLabel.transform.position = screenPosition;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Destruir el nombre de usuario cuando el jugador se destruye
+        if (nameLabel != null)
+        {
+            Destroy(nameLabel);
         }
     }
 }
